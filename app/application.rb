7341b27d4fc24d43f1b5a721a8f4f6bbaa327164
Christine Tran <christinetran825@gmail.com>
2017-10-12 22:55:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,13 +3,22 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
-
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
+  #   it 'wishes user a good morning in the morning' do
+  #     allow(Time).to receive(:now){
+  #       Time.new(2015,11,27,9,30)
+  #     }
+  #     get '/'
+  #     expect(last_response.body).to include("Morning")
+  #   end
+  #
+  #   it 'wishes user a good afternoon in the afternoon' do
+  #     allow(Time).to receive(:now){
+  #       Time.new(2015,11,27,14,30)
+  #     }
+  #     get '/'
+  #     expect(last_response.body).to include("Afternoon")
+  #   end
+  # end
 
     if num_1==num_2 && num_2==num_3
       resp.write "Morning"
